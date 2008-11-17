80eddd1ae015c092bd31f47eb458534166020d06
Alex Mattern <mattea@rpi.edu>
2008-11-17 02:06:06 +0000

    I am testing out my new PHP/SQL skills. A basic addinventory page.

---
+++ consumeinventory.php
@@ -0,0 +1,9 @@
+<?php
+	$value = $_POST['value'];
+	$location = $_POST['location'];
+	$condition = $_POST['condition'];
+	$description = $_POST['description'];
+	$db = mysql_connect("localhost", "inventory", "1nvp@ss") or die("Could not connect: " . mysql_error());
+	mysql_select_db("inventory", $db) or die("Database not selectable:" . mysql_error();
+	mysql_query("INSERT INTO `data` VALUES ('$description', '$location', '$condition', '$value')");
+?>
