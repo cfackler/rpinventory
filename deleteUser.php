fa7791bfc621cf21d8a01697720faeb0e71d5872
Daniel Delaney <deland@rpi.edu>
2008-11-20 22:22:51 +0000

    Removed PHP conformation for user delete. (Now done via javascript)

--- deleteUser.php
+++ deleteUser.php
@@ -17,20 +17,6 @@ if($auth != 2)
 $id = (int)$_GET["id"];
 if($id == 0)
 	die("Invalid ID");
-
-
-$sure = $_GET['sure'];
-//Make sure they want to delete
-if($sure != "yes")
-{
-	echo "<h1>Are you sure?</h1>";
-	echo "<a href=\"deleteUser.php?id=" . $id . "&sure=yes\">Yes</a><br><br>";
-	echo "<a href=\"manageusers.php\">No</a><br>";
-	
-	die("");
-}
-
-
 	
 //Create query
 $sql = "Delete from logins where id = '" . $id . "'";
