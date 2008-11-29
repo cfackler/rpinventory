1560780850a83093897bb39450d93f27cb5bfbd1
Jason Bartley <bartlj2@rpi.edu>
2008-11-29 17:07:42 +0000

    First attempt at adding a business.

---
+++ insertBusiness.php
@@ -0,0 +1,66 @@
+<?php
+require_once("inc/connect.php");  //mysql
+require_once("inc/auth.php");  //Session
+
+$link = connect();
+if($link == null)
+	die("Database connection failed");
+
+//Authenticate
+$auth = GetAuthority();	
+
+if($auth<1)
+	die("Please login to complete this action");
+
+//Company name
+$company = $_POST["company"];
+if(strlen($company) == 0)
+	die("Must have a name");
+	
+//Address
+$address = $_POST["address"];
+if(strlen($address) == 0)
+	die("Must have an address");
+
+$address2 = $_POST["address2"];
+
+//City
+$city = $_POST["city"];
+if(strlen($city) == 0)
+	die("Must have a city");
+
+//State
+$state = $_POST["state"];
+if(strlen($state) == 0)
+	die("Must have a state");
+
+//Zip Code
+$zip = $_POST["zip"];
+if(strlen($zip) == 0)
+	die("Must have a zip code");
+
+//Contact info
+$phone = $_POST["phone"];
+$fax = $_POST["fax"];
+$email = $_POST["email"];
+if(strlen($phone) == 0 && strlen($fax) == 0 && strlen($email) == 0)
+	die("Must have contact information");
+
+$website = $_POST["website"];
+
+$query = "insert into addresses (address_id, address, address2, city, state, zipcode, phone) VALUES(NULL, '" . $address . "', '" . $address2 . "', '" . $city . "', '" . $state . "', '" . $zip . "', '" . $phone . "')";
+		
+if(!mysqli_query($link, $query))
+	die("Query failed");
+$address_id = mysqli_insert_id($link);
+
+$sql = "INSERT INTO businesses (business_id, address_id, company name, fax, email, website) VALUES (NULL, '" . $address_id . "' , '" . $company . "', '" . $fax . "', '" . $email . "', '" . $website . "')";
+
+	
+if(!mysqli_query($link, $sql))
+	die("Query failed");
+
+mysqli_close($link);
+header('Location: manageLocations.php');
+	
+?>
--- templates/addBusiness.tpl
+++ templates/addBusiness.tpl
@@ -2,57 +2,57 @@
 
 <h3>Add Business</h3>
 
-<table width="400">
+<table width="410">
 
 
 <tr>
 	<td>Company Name: </td>
-	<td><input type="text" name="location" size="40"></td>
+	<td><input type="text" name="company" size="40"></td>
 </tr>
 
 <tr>
 	<td>Address: </td>
-	<td><input type="text" name="location" size="40"></td>
+	<td><input type="text" name="address" size="40"></td>
 </tr>
 
 <tr>
 	<td>Address 2: </td>
-	<td><input type="text" name="location" size="40"></td>
+	<td><input type="text" name="address2" size="40"></td>
 </tr>
 
 <tr>
 	<td>City: </td>
-	<td><input type="text" name="location" size="40"></td>
+	<td><input type="text" name="city" size="40"></td>
 </tr>
 
 <tr>
 	<td>State: </td>
-	<td><input type="text" name="location" size="10"></td>
+	<td><input type="text" name="state" size="10"></td>
 </tr>
 
 <tr>
 	<td>Zip Code: </td>
-	<td><input type="text" name="location" size="10"></td>
+	<td><input type="text" name="zip" size="10"></td>
 </tr>
 
 <tr>
 	<td>Phone Number: </td>
-	<td><input type="text" name="location" size="20"></td>
+	<td><input type="text" name="phone" size="20"></td>
 </tr>
 
 <tr>
 	<td>Fax Number: </td>
-	<td><input type="text" name="location" size="20"></td>
+	<td><input type="text" name="fax" size="20"></td>
 </tr>
 
 <tr>
 	<td>E-mail: </td>
-	<td><input type="text" name="location" size="40"></td>
+	<td><input type="text" name="email" size="40"></td>
 </tr>
 
 <tr>
 	<td>Website: </td>
-	<td><input type="text" name="location" size="40"></td>
+	<td><input type="text" name="website" size="40"></td>
 </tr>
 
 </table>
