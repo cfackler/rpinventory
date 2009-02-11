0369fe214dcd61183df4aaf6a893da0b0a3c20ef
Cameron Fackler <facklc@rpi.edu>
2009-02-11 12:04:28 -0500

    Sanitize user input. Clean up formatting. Check authentication before opening database connection.

--- insertLocation.php
+++ insertLocation.php
@@ -2,55 +2,55 @@
 
 /*
 
-    Copyright (C) 2008, All Rights Reserved.
+  Copyright (C) 2008, All Rights Reserved.
 
-    This file is part of RPInventory.
+  This file is part of RPInventory.
 
-    RPInventory is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+  RPInventory is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
 
-    RPInventory is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+  RPInventory is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with RPInventory.  If not, see <http://www.gnu.org/licenses/>.
+  You should have received a copy of the GNU General Public License
+  along with RPInventory.  If not, see <http://www.gnu.org/licenses/>.
 
 */
 
 require_once("inc/connect.php");  //mysql
 require_once("inc/auth.php");  //Session
 
-$link = connect();
-if($link == null)
-	die("Database connection failed");
-
 //Authenticate
 $auth = GetAuthority();	
-
 if($auth<1)
-	die("Please login to complete this action");
+  die("Please login to complete this action");
+
+$link = connect();
+if($link == null)
+  die("Database connection failed");
 
 //Description
 $desc = $_POST["description"];
 if(strlen($desc) == 0)
-	die("Must have a description");
+  die("Must have a description");
 	
 //Location
 $location = $_POST["location"];
 if(strlen($location) == 0)
-	die("Must have a location");
+  die("Must have a location");
 
+// Clean user input
+$desc = mysqli_real_escape_string($link, $desc);
+$location = mysqli_real_escape_string($link, $location);
 
-	
 $sql = "INSERT INTO locations (location_id, location, description) VALUES (NULL, '" . $location . "', '" . $desc . "')";
-
 	
 if(!mysqli_query($link, $sql))
-	die("Query failed");
+  die("Query failed");
 
 mysqli_close($link);
 header('Location: manageLocations.php');
