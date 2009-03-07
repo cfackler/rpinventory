01a7297231dbdf6fa400d9c4806f0c129bded7ff
Cameron Fackler <facklc@rpi.edu>
2009-03-07 07:12:16 -0500

    Remove unnecessary include, fix formatting.

--- logout.php
+++ logout.php
@@ -2,28 +2,26 @@
 
 /*
 
-    Copyright (C) 2008, All Rights Reserved.
+  Copyright (C) 2008, All Rights Reserved.
 
-    This file is part of RPInventory.
+  This file is part of RPInventory.
 
-    RPInventory is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+  RPInventory is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
 
-    RPInventory is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+  RPInventory is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with RPInventory.  If not, see <http://www.gnu.org/licenses/>.
+  You should have received a copy of the GNU General Public License
+  along with RPInventory.  If not, see <http://www.gnu.org/licenses/>.
 
 */
 
-require_once("inc/connect.php");  //mysql
 require_once("inc/auth.php");  //authentication
-
 
 Logout();
 
