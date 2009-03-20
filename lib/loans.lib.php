0443339764c84e336b5c90c1d30993cef6eaea36
Josh Elser <elserj@rpi.edu>
2009-03-20 18:46:34 -0400

    Added functionality to summarize loans given two dates

---
+++ lib/loans.lib.php
@@ -0,0 +1,59 @@
+<?php
+/*
+
+  Copyright (C) 2008, All Rights Reserved.
+
+  This file is part of RPInventory.
+
+  RPInventory is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  RPInventory is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with RPInventory.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+//inventory.lib.php
+require_once("inc/connect.php");  //mysql
+require_once("inc/auth.php");  //Session
+
+/* Takes two dates, formatted as YYYY-MM-DD */
+function getLoans($startDate, $endDate)
+{
+  // Connect
+  $link = connect();
+  if( $link == null )
+    die( "Database connection failed" );
+  
+  // Authenticate
+  $auth = GetAuthority();
+
+  $startDate = mysqli_real_escape_string( $link, $startDate );
+  $endDate = mysqli_real_escape_string( $link, $endDate);
+  
+  // Loan History
+  $query= "SELECT issue_date, return_date, starting_condition, inventory.description, username FROM loans, inventory, logins WHERE logins.id = loans.borrower_id AND loans.inventory_id = inventory.inventory_id AND issue_date >= '". $startDate ."' AND (return_date <= '". $endDate ."' OR return_date IS NULL)";
+
+  $result = mysqli_query($link, $query) or
+    die( 'Could not get the loan history' );
+
+  $records = array();
+  
+  while($record = mysqli_fetch_object($result))
+    {
+      $records [] = $record;
+    }
+  
+  mysqli_close($link);	
+  
+  return $records;
+}
+
+?>
--- lib/pdf.lib.php
+++ lib/pdf.lib.php
@@ -23,8 +23,12 @@
 
 
 require_once('modules/pdf-php/class.ezpdf.php');
-require_once('lib/inventory.lib.php');	
-	
+require_once('lib/inventory.lib.php');
+require_once('lib/loans.lib.php');
+
+function generatePDF(){
+  
+
 function getInventoryPDF()
 {
 	$items = getInventory();
@@ -56,5 +60,51 @@ function getInventoryPDF()
 	//output document
 	$pdf->ezStream();
 
+}
+
+function getLoanPDF()
+{
+  $then = date('Y-m-d', time() - (60*60*24*30*6));
+  $now = date('Y-m-d', time());
+
+  $records = getLoans( $then, $now );
+  
+  //create new pdf document
+  $pdf =& new Cezpdf("paper='LETTER'");
+  
+  //choose font
+  $pdf->selectFont('modules/pdf-php/fonts/Helvetica.afm');
+  
+  //start page numbers
+  $pdf->ezStartPageNumbers(300, 50, 10);
+  
+  //add text
+  $pdf->ezText('<u>Clubname Borrowing History</u>', 12, array('justification'=>'center'));
+  $pdf->ezText('<u>'. $then .' to '. $now .'</u>', 10, array('justification'=>'center'));
+  $pdf->ezText('');
+  
+  $data = array();
+  foreach($records as $value)
+    {
+      /*     echo "'";
+      print_r( $value->return_date);
+      echo "'";
+      echo "<br/>";*/
+      if( $value->return_date == '' ){ /* Not yet returned */
+	$value->return_date = "Outstanding";
+      }
+
+      $data [] = array('Item' => $value->description,
+		       'Condition' => $value->starting_condition,
+		       'User' => $value->username,
+		       'Date Issued' => $value->issue_date,
+		       'Date Returned' => $value->return_date);
+    }
+  
+  $pdf->ezTable($data);
+  
+  //output document
+  $pdf->ezStream();
+  
 }
 ?>
