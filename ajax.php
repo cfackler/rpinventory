982defb18db6522d29bd84edf8f9ee83ce259790
Josh Elser <elserj@rpi.edu>
2009-03-30 10:29:54 -0400

    Adding this file to the other branch

---
+++ ajax.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+
+    Copyright (C) 2008, All Rights Reserved.
+
+    This file is part of RPInventory.
+
+    RPInventory is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    RPInventory is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with RPInventory.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+require_once('lib/locations.lib.php');
+
+switch ($_GET["operation"])
+{
+	case "locations":
+		print getLocationsOptions();
+		break;
+  case "savelocation":
+    insertLocation($_GET["location"], $_GET["description"]);
+    break;
+}
+
+
+?>
