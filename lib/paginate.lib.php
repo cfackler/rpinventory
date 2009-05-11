48cb17016ef10f845ae648a204cdf84c46e8e9b8
Josh Elser <elserj@rpi.edu>
2009-05-11 14:55:26 -0400

    Whoops, forgot to change the pagination number back

--- lib/paginate.lib.php
+++ lib/paginate.lib.php
@@ -28,7 +28,7 @@ function paginate( $smarty, $itemVarName, $currentSortIndex, $currentSortDir, $m
 
   SmartyPaginate::disconnect();	/* Remove the old session data first, or URL's are wrong */
   SmartyPaginate::connect();
-  SmartyPaginate::setLimit( 2 );
+  SmartyPaginate::setLimit( 15 );
 
   $smarty->assign( $itemVarName, getPaginatedResults( $itemVarName, 
 						 $currentSortIndex,
