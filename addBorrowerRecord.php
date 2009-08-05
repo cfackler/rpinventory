af8c9c28aaef9edebf1bd5a73c60ce9e9c87216a
Josh Elser <elserj@rpi.edu>
2009-08-04 20:55:43 -0400

    Change the sql queries

--- addBorrowerRecord.php
+++ addBorrowerRecord.php
@@ -82,14 +82,6 @@ $state = mysqli_real_escape_string($link, $state);
 $zip = mysqli_real_escape_string($link, $zip);
 $phone = mysqli_real_escape_string($link, $phone);
 
-// Insert the borrower
-$sql = "INSERT INTO borrowers (borrower_id , rin, email, name) VALUES (NULL, '" . $rin . "', '" . $email . "', '" . $name ."')";	
-
-if(!mysqli_query($link, $sql))
-  die("Query failed");
-
-$borrower_id = mysqli_insert_id($link);
-
 // Insert the address
 $sql = "INSERT INTO addresses (address_id, address, address2, city, state, zipcode, phone) VALUES (NULL, '". $address ."', '". $address2 ."', '". $city ."', '". $state ."', '". $zip ."', '". $phone ."')";
 
@@ -98,11 +90,11 @@ if(!mysqli_query($link, $sql))
 
 $address_id = mysqli_insert_id($link);
 
-// Insert the link between address and borrower
-$sql = "INSERT INTO borrower_addresses (user_id, address_id) VALUES (". $borrower_id .", ". $address_id .")";
+// Insert the borrower
+$sql = "INSERT INTO borrowers (borrower_id, address_id, rin, email, name) VALUES (NULL, ". $address_id .", '" . $rin . "', '" . $email . "', '" . $name ."')";	
 
 if(!mysqli_query($link, $sql))
-	die('Query failed');
+  die("Query failed");
 
 mysqli_close($link);
 header('Location: manageBorrowers.php');
