8824d07a59db6f5fd54795c572ca49acde5d803f
Josh Elser <elserj@rpi.edu>
2009-12-21 23:50:07 -0500

    Update getAddressFromBorrower function and add updateAddress function

--- lib/addresses.lib.php
+++ lib/addresses.lib.php
@@ -21,31 +21,33 @@
 */
 
 /* Returns the id of the given borrower */
-function getAddressFromBorrower( $id ) {
-	require_once('lib/connect.lib.php');
-	require_once('lib/auth.lib.php');
+function getAddressFromBorrower($borrower_id)
+{
+    require_once('class/database.class.php');
 
-	// Database
-	$link = connect();
-	if( $link == null )
-		die( 'Database connection failed' );
+    $db = new database();
 
-	// Authority
-	$auth = GetAuthority();
+    if (!isset($_SESSION['club']))
+    {
+        return 0;
+    }
 
-	// Sanitize
-	$id = (int)$id;
-	if($id == 0)
-		die('Invalid ID');
+    $club_id = $_SESSION['club'];
 
-	$sql = 'SELECT address_id FROM borrowers WHERE borrower_id = ' . $id;
+    $sql = 'SELECT *  FROM addresses, borrowers WHERE addresses.address_id = borrowers.address_id and borrowers.borrower_id = ? LIMIT 1';
 
-	$result = mysqli_query($link, $sql) or
-		die( 'Query Failed' );
+    $result = $db->query($sql, $borrower_id);
 
-	$address_id = mysqli_fetch_object($result);
+    if (mysqli_num_rows($result) == 0)
+    {
+        return NULL;
+    }
 
-	return $address_id->address_id;
+    $obj = $db->getObject($result);
+
+    $db->close();
+
+    return $obj;
 }
 
 /* Returns the address with the given id */
@@ -98,6 +100,26 @@ function addAddress($address, $address2, $city, $state, $zipcode, $phone)
     $db->close();
 
     return $id;
+}
+
+function updateAddress($address_id, $address, $address2, $city, $state, $zipcode, $phone)
+{
+    require_once('class/database.class.php');
+
+    $db = new database();
+
+    if (!isset($_SESSION['club']))
+    {
+        return 0;
+    }
+
+    $club_id = $_SESSION['club'];
+
+    $sql = 'UPDATE addresses SET address = ?, address2 = ?, city = ?, state = ?, zipcode = ?, phone = ? WHERE address_id = ?';
+
+    $db->query($sql, $address, $address2, $city, $state, $zipcode, $phone, $address_id);
+
+    $db->close();
 }
 
 ?>
