ebdc073edce9d97948534df19dd207383ecab6f7
Josh Elser <elserj@rpi.edu>
2009-12-25 22:28:17 -0500

    Selectively require files

--- ajax.php
+++ ajax.php
@@ -21,61 +21,77 @@
 
  */
 
-require_once( 'lib/locations.lib.php' );
-require_once( 'lib/users.lib.php' );
-require_once( 'lib/businesses.lib.php' );
-require_once( 'lib/borrowers.lib.php' );
-require_once( 'lib/display.lib.php' );
-require_once( 'lib/categories.lib.php' );
 require_once('lib/auth.lib.php');
 
 switch ($_GET['operation'])
 {
 case 'locations':
+    require_once( 'lib/locations.lib.php' );
+
     print getLocationsOptions();
     break;
 
 case 'businesses':
+    require_once( 'lib/businesses.lib.php' );
+
     print getBusinessesOptions();
     break;
 
 case 'loanlocations':
+    require_once( 'lib/locations.lib.php' );
+
     print getLoanLocationsOptions();
     break;
 
 case 'savelocation':
+    require_once( 'lib/locations.lib.php' );
+
     print insertLocation($_GET['location'], $_GET['description']);
     break;
 
 case 'saveBusiness':
+    require_once( 'lib/businesses.lib.php' );
+
     print insertBusiness($_POST['company_name'], $_POST['address'], $_POST['address2'],
         $_POST['city'], $_POST['state'], $_POST['zipcode'], $_POST['phone'],
         $_POST['fax_number'], $_POST['email'], $_POST['website']);
     break;
 
 case 'saveBorrower':
+    require_once( 'lib/borrowers.lib.php' );
+
     insertBorrower($_GET['borrower_name'], $_GET['rin'], $_GET['email'],
         $_GET['address'], $_GET['address2'], $_GET['city'],
         $_GET['state'], $_GET['zipcode'], $_GET['phone']);
     break;		
 
 case 'username':
+    require_once( 'lib/users.lib.php' );
+
     print getUsernames( $_GET['name'] );
     break;
 
 case 'borrowerNames':
+    require_once( 'lib/borrowers.lib.php' );
+
     print getBorrowerNames( $_GET['name'] );
     break;
 
 case 'insertCategory':
+    require_once( 'lib/categories.lib.php' );
+
     print insertCategory($_GET['category_name']);
     break;
 
 case 'options':
+    require_once( 'lib/display.lib.php' );
+    
     print get_options($_GET['table_name'], $_GET['value_column'], $_GET['display_column']);
     break;
 
 case 'itemCategoryIDs':
+    require_once( 'lib/categories.lib.php' );
+
     if(isset($_POST['store']))
         print get_item_category_ids($_POST['inventory_id'], $_POST['store']);
     else
@@ -83,6 +99,8 @@ case 'itemCategoryIDs':
     break;
 
 case 'borrowerIdFromName':
+    require_once( 'lib/borrowers.lib.php' );
+
     print getBorrowerId($_GET['name'], $_GET['club_id']);
     break;
 }
