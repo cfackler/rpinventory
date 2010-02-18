d7efdca04ef0264c1b4c1aae09f83a0d2bb57ba1
Josh Elser <elserj@rpi.edu>
2010-02-17 19:15:21 -0500

    Add files for adding a custom field

---
+++ addCustomField.php
@@ -0,0 +1,58 @@
+<?php
+
+/*
+
+    Copyright (C) 2010, All Rights Reserved.
+
+    This file is part of RPInventory.
+
+    RPInventory is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    RPInventory is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with RPInventory.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+
+require_once('lib/auth.lib.php');  //Session
+require_once('lib/fields.lib.php');
+require_once('class/database.class.php');
+
+// Connect
+$db = new database();
+
+//Authenticate
+$auth = GetAuthority();	
+if ($auth < 2)
+{
+	die("You dont have permission to access this page");
+}
+
+require_once('lib/smarty_inv.class.php');
+
+$smarty = new Smarty_Inv();
+
+//Assign vars
+$smarty->assign('title', "Add Custom Field");
+$smarty->assign('authority', $auth);
+$smarty->assign('page_tpl', 'addCustomField');
+$smarty->assign('users', $users);
+
+if (count($users) == 0)
+{
+    $smarty->assign('emptyTable', TRUE);
+}
+
+$smarty->display('index.tpl');
+
+$db->close();
+
+?>
